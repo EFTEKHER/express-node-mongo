8f8c53891e57af8688c864a00db79334cd1cf574
EFTEKHER <eftekherali2000@gmail.com>
2024-06-09 14:12:42 +0600

    done

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const http = require('http');
-const server = http.createServer();
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
+    if(req.url==='/')
+        {
+            res.end('This is a test')
+        }
+});
+const fs = require('fs');
 
 
-const port = 1000;
+const port = 5000;
 
+const fileRead= fs.readFileSync('./sample.txt','utf-8');
+console.log(fileRead);
+
+
+fs.writeFile('index.txt',"Hello World",()=>{
+    console.log('file written')
+})
 server.listen(port,()=>{
     console.log(`server listening on port ${port}`);
-    
+
 });
